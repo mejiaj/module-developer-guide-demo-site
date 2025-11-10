dc65dca3d82a47305e883a728224a6d9f844201d
James Mejia <s.jamesmejia@gmail.com>
2025-11-10 13:50:18 -0600

    Add params to route
    Allow routes to use a dynamic path to serve two urls.

--- web/modules/anytown/src/Controller/WeatherPage.php
+++ web/modules/anytown/src/Controller/WeatherPage.php
@@ -14,13 +14,21 @@ class WeatherPage extends ControllerBase {
   /**
    * Builds the response.
    */
-  public function build(): array {
-
+  public function build(string $style): array {
+    // Style should be one of 'short' (default) or 'extended.'
+    $style = (in_array($style, ['short', 'extended'])) ? $style : 'short';
 
     $build['content'] = [
       '#type' => 'markup',
       '#markup' => '<p>The weather forecase for this week is sunny with a chance of meatballs.</p>',
     ];
+
+    if ($style === 'extended') {
+      $build['content_extended'] = [
+        '#type' => 'markup',
+        '#markup' => '<p><strong>Extended forecase: </strong> Looking ahead to next week we expect some snow.</p>',
+      ];
+    }
 
     return $build;
   }
